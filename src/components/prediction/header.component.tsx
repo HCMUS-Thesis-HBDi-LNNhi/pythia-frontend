e13a47dc50469e6139cdcf1bdc9abf41b74b807a
NgocNhi123 <nnhily16@gmail.com>
2022-04-24 15:55:16 +0700

    create prediction page layout

---
+++ src/components/prediction/header.component.tsx
@@ -0,0 +1,38 @@
+import { Button, Tag } from "components/common";
+import { useState } from "react";
+import { TagColor } from "interfaces/common.interface";
+import { AcquireModel } from "interfaces/prediction.interface";
+
+export default function Header(): JSX.Element {
+  const [selectModel, setSelectModel] = useState<AcquireModel>(
+    AcquireModel.clustering
+  );
+
+  return (
+    <main
+      className={[
+        "border border-gray-300 shadow rounded-lg",
+        "p-4 space-y-4",
+      ].join(" ")}
+    >
+      <div className="flex justify-end items-center w-full">
+        <label htmlFor="model">
+          <strong>Models: </strong>
+        </label>
+        <select
+          id="model"
+          className="border p-2 ml-2 mr-8 min-w-[12rem] rounded border-primary-500"
+          value={selectModel}
+          onChange={(e) => setSelectModel(parseInt(e.target.value))}
+        >
+          <option value={AcquireModel.clustering}>Clustering</option>
+        </select>
+        <div className="space-x-2 mr-auto">
+          <strong>Status: </strong>
+          <Tag color={TagColor.blue}>In Progress</Tag>
+        </div>
+        <Button style="solid">Upload data</Button>
+      </div>
+    </main>
+  );
+}
