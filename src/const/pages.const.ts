2a462ef88dec3448cda68285e71b752099e12588
NgocNhi123 <nnhily16@gmail.com>
2022-05-11 23:59:05 +0700

    fix deploy error

--- src/const/pages.const.ts
+++ src/const/pages.const.ts
@@ -2,8 +2,8 @@ import { IMenuItem, PageLabels } from "interfaces/common.interface";
 import icons from "./icons.const";
 
 export const menuItems: IMenuItem[] = [
+  { label: PageLabels.PROFILE, icon: icons.solid.account },
   { label: PageLabels.DASHBOARD, icon: icons.solid.home },
   { label: PageLabels.ANALYTICS, icon: icons.outline.analytic },
   { label: PageLabels.PREDICTION, icon: icons.solid.prediction },
-  { label: PageLabels.PROFILE, icon: icons.solid.account },
 ];
