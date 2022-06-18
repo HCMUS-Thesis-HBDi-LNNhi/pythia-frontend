6f76d96e54c44edfcdcaf07ee4dccd360525fdc2
NgocNhi123 <nnhily16@gmail.com>
2022-06-18 14:08:09 +0700

    create instruction dialog

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,5 @@
-import { Layout } from "components/common";
+import { Layout, Dialog, UploadButton, Button } from "components/common";
 import { ChartList } from "components/sections/home";
-import Instructions from "components/sections/instructions";
 import { IData } from "interfaces/data.interface";
 import { useEffect, useState } from "react";
 import { useReadLocalStorage } from "usehooks-ts";
@@ -8,7 +7,7 @@ import { handleFetchData } from "utils/handleData";
 
 export default function Home(): JSX.Element {
   const userID = useReadLocalStorage<string>("user-id");
-  const [firstUser, _setFirstUser] = useState(false);
+  const [firstUser, setFirstUser] = useState(true);
   const [isLoading, setLoading] = useState(false);
   const [data, setData] = useState<IData>();
 
@@ -26,7 +25,27 @@ export default function Home(): JSX.Element {
       >
         {data && <ChartList setLoading={setLoading} data={data} />}
       </Layout>
-      {firstUser && <Instructions />}
+      {firstUser && (
+        <Dialog className="text-primary-700 p-6 space-y-4">
+          <h1 className="text-2xl font-bold text-center">Instruction</h1>
+          <h3 className="text-l font-medium italic">
+            Hello, before continue, please:
+          </h3>
+          <div>1. Upload customer transactions:</div>
+          <div className="grid place-content-center">
+            <UploadButton setLoading={setLoading} fileType="transaction" />
+          </div>
+          <div>2. Upload customer demographic data:</div>
+          <div className="grid place-content-center">
+            <UploadButton setLoading={setLoading} fileType="demographic" />
+          </div>
+          <div className="grid place-content-center pt-6">
+            <Button style="solid" onClick={() => setFirstUser(false)}>
+              Finish
+            </Button>
+          </div>
+        </Dialog>
+      )}
     </>
   );
 }
