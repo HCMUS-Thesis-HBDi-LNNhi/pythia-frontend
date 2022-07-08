e2c4eaab470a4ed403128081de51007b52121925
NgocNhi123 <nnhily16@gmail.com>
2022-07-08 16:30:51 +0700

    update utils

--- src/utils/formatter.utils.ts
+++ src/utils/formatter.utils.ts
@@ -1,3 +1,5 @@
+import { CONTINENTS, COUNTRIES } from "const/country-code.const";
+
 export const formatDate = (
   date: Date,
   formatType: string = "en-AU"
@@ -7,4 +9,16 @@ export const formatDate = (
     timeStyle: "medium",
   }).format(date);
   return dateString + ", " + timeString;
+};
+
+export function removeDuplicateObjects<T>(array: T[]): T[] {
+  return Array.from(
+    new Set(array.map((value: T) => JSON.stringify(value)))
+  ).map((value: string) => JSON.parse(value));
+}
+
+const countries = Object.values(COUNTRIES);
+export const getContinentCode = (name: string) => {
+  const result = countries.find((value) => value.name === name);
+  return result && (result.continent as keyof typeof CONTINENTS);
 };
